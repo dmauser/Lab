fca7d57220b958729132ec5b08b434b48acab340
Daniel Mauser <dmauser@hotmail.com>
2021-07-21 16:45:37 -0500

    update

--- AzureVM-nettools/nettools.sh
+++ AzureVM-nettools/nettools.sh
@@ -1,17 +1,17 @@
 # This script has been tested on Ubuntu
 # Update repository
-sudo apt update -y && sudo apt upgrade -y 
+apt update -y && sudo apt upgrade -y 
 #Traceroute
-sudo apt-get install traceroute -y
+apt-get install traceroute -y
 # TCP traceroute
-sudo apt-get install tcptraceroute -y
+apt-get install tcptraceroute -y
 # Nmap
-sudo apt-get install nmap -y
+apt-get install nmap -y
 # Hping3
-sudo apt-get install hping3 -y
+apt-get install hping3 -y
 # iPerf
-sudo apt-get install iperf3 -y
+apt-get install iperf3 -y
 # Nginx and adds machine name on main page
-sudo apt-get install nginx -y && hostname > /var/www/html/index.html
+apt-get install nginx -y && hostname > /var/www/html/index.html
 # Speedtest
-sudo apt-get install speedtest-cli -y
+apt-get install speedtest-cli -y
